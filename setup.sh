55cc5ef74df5e634cd8c75149f017611d4b68235
Afif Albana <albana.afif.m@gmail.com>
2021-04-24 22:20:50 +0700

    Add phpLDAPadmin vars

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@
 source variables.sh
 source functions.sh
 
-# Update and upgrade software packages.
-yum -y update && yum -u upgrade
+# Install epel, update and upgrade software packages.
+yum -y install epel-release && yum -y update && yum -u upgrade
 
 # Install OpenLDAP-server and dependencies .
 yum -y install openldap \
@@ -29,3 +29,19 @@ ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
 # Enable LDAP log to rsyslog
 echo "local4.* /var/log/ldap.log" >> /etc/rsyslog.conf
 systemctl restart rsyslog
+
+# Install phpLDAPAdmin from EPEL repository.
+yum -y phpldapadmin
+
+# Grant phpLDAPadmin to be access from all network.
+sed -i -e 's/Require local/Require all granted/' /etc/httpd/conf.d/phpldapadmin.conf
+systemctl restart httpd
+
+# Configure phpLDAPAdmin.
+CONF_FILE=/etc/phpldapadmin/config.php
+sed -i -e "s/Local LDAP Server/$LDAP_SERVER_NAME/" $CONF_FILE
+sed -i -e "s/$servers->setValue('login','attr','uid');/\/\/ $servers->setValue('login','attr','uid');" $CONF_FILE
+echo "$servers->setValue('server','host','$LDAP_HOST');" >> $CONF_FILE
+echo "$servers->setValue('server','port', $LDAP_PORT);" >> $CONF_FILE
+echo "$servers->setValue('server','base',array('$LDAP_BASE'));" >> $CONF_FILE
+echo "$servers->setValue('login','attr','dn');" >> $CONF_FILE
